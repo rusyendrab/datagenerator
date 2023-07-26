7282815b5279a95b9333a39a6e78a13807e97d79
rusyendrab <140568779+rusyendrab@users.noreply.github.com>
2023-07-25 23:39:41 -0400

    src

---
+++ src/main/java/com/datagenerator/AppConfigs.java
@@ -0,0 +1,7 @@
+package com.datagenerator;
+
+public class AppConfigs {
+    public final static String applicationID = "datagenerator";
+    //public final static String bootstrapServers = "localhost:9092,localhost:9093";
+    public final static String bootstrapServers = "localhost:9092";
+}
